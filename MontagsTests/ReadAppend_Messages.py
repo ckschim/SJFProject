ea739ee12379f59f1f2ba7306c992ebccd6a0d2c
abrogans <wayne.roettger@bluewin.ch>
2019-09-09 16:24:39 +0200

    Implemented append_messages

--- MontagsTests/ReadAppend_Messages.py
+++ MontagsTests/ReadAppend_Messages.py
@@ -6,6 +6,12 @@ encrypted = box.encrypt
 def append_messages(message, box, file="Messages.txt"):
     print(file)
 
+    encrypted_message = box.encrypt(message)
+
+    with open(file, "a") as myfile:
+        myfile.write(encrypted_message)
+        myfile.write("\n")
+
 
 def read_messages(box, file="Messages.txt"):
     print(file)
