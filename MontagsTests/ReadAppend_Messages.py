61dbb6a0d9634655749a9974c0eba43777d07798
abrogans <wayne.roettger@bluewin.ch>
2019-09-09 17:02:54 +0200

    Converted string to bytes

--- MontagsTests/ReadAppend_Messages.py
+++ MontagsTests/ReadAppend_Messages.py
@@ -4,7 +4,7 @@ from nacl.public import Box
 def append_messages(message, box, file="Messages.txt"):
     print(file)
 
-    encrypted_message = box.encrypt(message)
+    encrypted_message = box.encrypt(bytes(message, 'utf-8'))
 
     with open(file, "a") as f:
         f.write(encrypted_message)
@@ -13,3 +13,11 @@ def append_messages(message, box, file="Messages.txt"):
 
 def read_messages(box, file="Messages.txt"):
     print(file)
+
+
+
+
+with open(filepath) as fp:
+   line = fp.readline()
+   while line:
+       line = fp.readline()
