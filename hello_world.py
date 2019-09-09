861a1843ce0f4ad8894d285cbd7f88f260b52fdc
Cedrik Schimschar <c.schimschar@fqsmedia.de>
2019-09-09 13:44:32 +0200

    PyNaCl hash example

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,14 @@
-print("hello world")
+import nacl.hash
+import nacl.encoding
+
+message = 'Hallo, ich mag Kuchen.'
+message2 = 'Kuchen.'
+
+hash_function = nacl.hash.sha256
+hash_function2 = nacl.hash.sha512
+
+output = hash_function(message.encode(), nacl.encoding.HexEncoder)
+output2 = hash_function2(message.encode(), nacl.encoding.HexEncoder)
+
+print(output)
+print(output2)
