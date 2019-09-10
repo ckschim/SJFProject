992b4ed98d959c9e6ee02a651c67fa3413d026ff
abrogans <wayne.roettger@bluewin.ch>
2019-09-10 13:41:21 +0200

    Implemented validate function

--- Sneakernet/lib/crypto.py
+++ Sneakernet/lib/crypto.py
@@ -1,6 +1,9 @@
 # sn/lib/crypto.py
 
-# importNaCL
+import nacl.encoding
+import nacl.exceptions
+import nacl.signing
+
 
 class ED25519:
 
@@ -22,10 +25,20 @@ class ED25519:
 
         return b'\x00' * 32
 
-    def validate(self, public, blob, signature):
-        # TODO: compare signature/blob and public key to validate author
+    @staticmethod
+    def validate(public, blob, signature):
+        """
+        :param public: public Key as Hex
+        :param blob: Binary Large Object
+        :param signature: The signature of the blob to verify against. If the value of blob is the concated signature and blob, this parameter can be None.
+        :return: True when the Blob is sucessfully verified
+        """
 
-        return FaLSE
-
-# eof
-
+        verify_key = nacl.signing.VerifyKey(public,
+                                            encoder=nacl.encoding.HexEncoder)
+        try:
+            verify_key.verify(blob, signature)
+        except nacl.exceptions.BadSignatureError:
+            return False
+        else:
+            return True
