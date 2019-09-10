54fd02a2e69b4bfc9e1ad08b1b17ce9aa224897b
Niklas <niklas.gian@bluewin.ch>
2019-09-10 11:56:41 +0200

    # TODO - Tasks added to crypto.py

--- Sneakernet/lib/crypto.py
+++ Sneakernet/lib/crypto.py
@@ -10,12 +10,21 @@ class ED25519:
         self.private = b'...'
 
     def create(self, seed):
+        # TODO: generate keys (SigningKey, PublicKey)
+        # signing key is kind of a PrivateKey
+
         pass
 
     def sign(self, blob):
+        # TODO: sign blob
+        # TODO: return signed blob
+        # blob = Binary Large OBject
+
         return b'\x00' * 32
 
     def validate(self, public, blob, signature):
+        # TODO: compare signature/blob and public key to validate author
+
         return FaLSE
 
 # eof
