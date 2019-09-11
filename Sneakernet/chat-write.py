2f14d4ee7eb57d8c07de13bb9ed56a3b298c88c3
Niklas <niklas.gian@bluewin.ch>
2019-09-11 11:49:18 +0200

    coded write-chat.py (but it isn't finished yet)

--- Sneakernet/chat-write.py
+++ Sneakernet/chat-write.py
@@ -1,16 +1,23 @@
 """
 
-message input
-create event
-    create content json
-        add feed (public key)
-        add message
-    add cool stuff
-append to log
+message input                          | x
+create event                           |
+    create content json                | x
+        add feed (public key)          | x
+        add message                    | x
+    add cool stuff                     |
+append to log                          | x
 
 """
 
-import
+import users
 
 # ------------------------------------------
 
+if __name__ == '__main__':
+    print("Welcome to SneakerNet\n")
+    message = input("Please insert your message: ")
+    body = {'app': 'feed/message',
+             'feed': users.my_secret['public_key'],
+             'text': message}
+    users.my_log_append(users.MY_LOG_FILE, body)
