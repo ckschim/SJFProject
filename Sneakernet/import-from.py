ab1eb2deaad0d915651374793b6cbdf4206649ec
tschudin <christian.tschudin@unibas.ch>
2019-09-11 06:38:36 +0200

    implement the export function, align output of import

--- Sneakernet/import-from.py
+++ Sneakernet/import-from.py
@@ -61,7 +61,7 @@ if __name__ == '__main__':
     import_dir = sys.argv[1]
     
     print("Welcome to SneakerNet\n")
-    print(f"** importing events from '{import_dir}'")
+    print(f"** importing new events from '{import_dir}'")
     print()
 
     if not os.path.isdir(import_dir):
@@ -86,10 +86,11 @@ if __name__ == '__main__':
             new_db[feed][seq].append(block)
             new_cnt += 1
         lg.close()
-    print(f"** found {new_cnt} event(s) to import")
+    print(f"** found {new_cnt} event(s) in '{import_dir}'")
 
     have_fn = {}
     have_max = {}
+    have_cnt = 0
     max_fn_number = 1
     for fn in os.listdir(LOGS_DIR):
         # remember highest file number, if we have to create a new file
@@ -99,6 +100,7 @@ if __name__ == '__main__':
 
         lg.open(os.path.join(LOGS_DIR, fn), 'r')
         for block in lg:
+            have_cnt += 1
             t.from_cbor(block)
             feed = t.event.feed
             if not feed in have_fn:
@@ -109,16 +111,21 @@ if __name__ == '__main__':
             if seq > have_max[feed]:
                 have_max[feed] = seq
         lg.close()
+    print(f"** found {have_cnt} event(s) in '{LOGS_DIR}'")
 
+    update_cnt = 0
     for feed in new_db:
         if not feed in have_fn:
             max_fn_number += 1
             have_fn[feed] = os.path.join(LOGS_DIR, str(max_fn_number)+'.pcap')
             have_max[feed] = 0
+            if update_cnt == 0:
+                print()
             print(f"** creating {have_fn[feed]} for {base64.b64encode(feed).decode('utf8')}")
             lg.open(have_fn[feed], 'w')
             lg.close()
             max_fn_number += 1
+            update_cnt += 1
 
     update_cnt = 0
     for feed in have_fn:
@@ -128,12 +135,14 @@ if __name__ == '__main__':
         # print(f"** testing {have_fn[feed]}, seq={have_max[feed]}")
         while have_max[feed]+1 in new_db[feed]:
             have_max[feed] += 1
+            if update_cnt == 0:
+                print()
             print(f"** import {base64.b64encode(feed).decode('utf8')}/{have_max[feed]}")
             lg.write(new_db[feed][have_max[feed]][0])
             update_cnt += 1
         lg.close()
 
     print()
-    print(f"** imported {update_cnt} event(s)")
+    print(f"** imported {update_cnt} event(s) to the '{LOGS_DIR}' directory")
 
 # eof
