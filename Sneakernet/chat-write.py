900b2a050d46e631533928d7a41542aeb187db8f
Niklas <niklas.gian@bluewin.ch>
2019-09-11 11:25:40 +0200

    create to-do chat-write

---
+++ Sneakernet/chat-write.py
@@ -0,0 +1,16 @@
+"""
+
+message input
+create event
+    create content json
+        add feed (public key)
+        add message
+    add cool stuff
+append to log
+
+"""
+
+import
+
+# ------------------------------------------
+
