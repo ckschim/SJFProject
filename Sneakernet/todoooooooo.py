be5d3cfe071c553bef4707d6f240ab1d5da6a839
Niklas <niklas.gian@bluewin.ch>
2019-09-11 15:35:44 +0200

    - only to be sure that everyone has the best version... cool :)

--- Sneakernet/todoooooooo.py
+++ Sneakernet/todoooooooo.py
@@ -1,17 +1,17 @@
 """
 
 message input                          | x
-create event                           |
+create event                           | x
     create content json                | x
         add feed (public key)          | x
         add message                    | x
     add cool stuff                     |
 append to log                          | x
 
-def output_chat():
+def output_chat():                     | x
 
-open all logs                          |
-print content(msg) in events           |
+open all logs                          | x
+print content(msg) in events           | x
 
 
 tdrzfugirafsduho= (11.11.19 / 12:34) :
